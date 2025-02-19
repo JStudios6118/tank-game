36444b6f718b84274471d045e043e4a59f6dc9e8
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2025-02-19 15:18:53 -0800

    idk

--- player.gd
+++ player.gd
@@ -3,7 +3,23 @@ extends CharacterBody2D
 @export var speed : int = 300
 @export var turnSpeed : int = 100
 
+@export var barrelTurnSpeed : int = 100
+
+@export var barrel : Node2D
+
 func _physics_process(delta: float) -> void:
+	var target_rotation = (get_global_mouse_position() - barrel.global_position).angle()
+	
+	# Calculate the difference between the current rotation and the target rotation.
+	var angle_diff = target_rotation - rotation
+	if angle_diff > PI:
+		angle_diff -= PI * 2
+	elif angle_diff < -PI:
+		angle_diff += PI * 2
+		
+	# Gradually rotate the barrel towards the target rotation.
+	barrel.rotation += angle_diff * barrelTurnSpeed * delta
+	
 	# Get the input direction and handle the rotation.
 	var direction := Input.get_axis("left", "right")
 	if direction:
