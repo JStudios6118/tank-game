1aedf1475717c93dd7c78cca00e8be6b84d3298d
JStudios6118 <99525426+JStudios6118@users.noreply.github.com>
2025-02-22 14:04:30 -0800

    idk

--- player.gd
+++ player.gd
@@ -7,6 +7,8 @@ extends CharacterBody2D
 
 @export var barrel : Node2D
 
+var turning: String = "No"
+
 func _physics_process(delta):
 	var angle_to_mouse = rad_to_deg(get_angle_to(get_global_mouse_position()))
 	var barrelRotation = barrel.rotation_degrees
@@ -17,7 +19,12 @@ func _physics_process(delta):
 	
 	# Rotate the barrel based on the angle difference and rotation amount
 	if abs(angle_diff) > rotation_amount:
-		barrel.rotation_degrees += sign(angle_diff) * rotation_amount
+		var turnExtra: int = 0
+		if turning == "Left":
+			turnExtra+=barrelTurnSpeed
+		elif turning == "Right":
+			turnExtra-=barrelTurnSpeed
+		barrel.rotation_degrees += sign(angle_diff) * (rotation_amount+turnExtra)
 	else:
 		barrel.rotation_degrees = angle_to_mouse
 
@@ -25,7 +32,13 @@ func _physics_process(delta):
 	# Get the input direction and handle the rotation.
 	var direction := Input.get_axis("left", "right")
 	if direction:
+		if direction < 0:
+			turning = "Left"
+		else:
+			turning = "Right"
 		rotate(direction * turnSpeed * delta)
+	else:
+		turning = "No"
 
 	if Input.is_action_pressed("forward"):
 		velocity = Vector2(cos(rotation), sin(rotation)) * speed
