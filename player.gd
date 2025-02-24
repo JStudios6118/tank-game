9dcf4cf0927009bb7cf471d4647c41d3fa388afb
LegacySK <as1456@pleasantonusd.net>
2025-02-23 17:02:04 -0800

    missile

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed : int = 300
 @export var turnSpeed : int = 100
 @export var body_pivot : Node2D
+@export var tank_missile : PackedScene
 
 @export var barrelTurnSpeed : int = 100
 
@@ -33,6 +34,13 @@ func _physics_process(delta):
 		velocity = Vector2(cos(body_pivot.rotation), sin(body_pivot.rotation)) * speed
 	else:
 		velocity = Vector2.ZERO
+	
+	if Input.is_action_just_pressed("shoot"):
+		var temp = tank_missile.instantiate()
+		temp.global_position = $BarrelEnd.global_position
+		temp.global_rotation = $BarrelEnd.global_rotation
+		get_tree().root.add_child(temp)
+
 
 	# Move the player in the direction they are looking.
 	move_and_slide()
